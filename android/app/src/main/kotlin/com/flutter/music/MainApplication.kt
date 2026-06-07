e754c3f8fac71fe2726008378a2145a0d62160d9
g-ai-126 <269969053+g-ai-126@users.noreply.github.com>
2026-06-07 14:05:57 +0800

    refactor(android): 将 MainActivity 迁移至 AudioServiceActivity，简化 Application 初始化

--- android/app/src/main/kotlin/com/flutter/music/MainApplication.kt
+++ android/app/src/main/kotlin/com/flutter/music/MainApplication.kt
@@ -1,11 +1,5 @@
 package com.flutter.music
 
 import android.app.Application
-import com.ryanheise.audioservice.AudioServicePlugin
 
-class MainApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        AudioServicePlugin.setFlutterEngineId("com.flutter.music.MainActivity")
-    }
-}
+class MainApplication : Application()
