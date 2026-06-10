10cd348dce51c4c98e91fe86b0d8b588749e7b43
g-ai-001 <g-ai-002@ganecheng.tech>
2026-06-10 15:29:44 +0800

    chore(版本): 升级版本号到 0.3.34

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -7,7 +7,7 @@ class AppConstants {
   AppConstants._();
 
   static const String appName = '音乐播放器';
-  static const String version = '0.3.33';
+  static const String version = '0.3.34';
 
   // SharedPreferences keys (playlists 列表 JSON)
   static const String prefKeyPlaylists = 'playlists_v1';
